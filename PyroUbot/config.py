4027129637209e818fc64a6e282aafd106e39ec2
zyyt49929-code <zyyt49929@gmail.com>
2025-12-27 16:31:12 +0800

    Update config.py

--- PyroUbot/config.py
+++ PyroUbot/config.py
@@ -7,9 +7,9 @@ MAX_BOT = int(os.getenv("MAX_BOT", "999"))
 
 DEVS = list(map(int, os.getenv("DEVS", "7369828453").split()))
 
-API_ID = int(os.getenv("API_ID", "25610"))
+API_ID = int(os.getenv("API_ID", "34297166"))
 
-API_HASH = os.getenv("API_HASH", "c02e1955293080d711883ef40ea")
+API_HASH = os.getenv("API_HASH", "a3ac0dc6d1185d91dc0018c4d1c99a8b")
 
 BOT_TOKEN = os.getenv("BOT_TOKEN", "8425610564:AAFeXvpnZtVkDSVny1J1-QzzIrdLpGcyxoU")
 
@@ -19,5 +19,5 @@ BLACKLIST_CHAT = list(map(int, os.getenv("BLACKLIST_CHAT", "-1002886184276").spl
 
 RMBG_API = os.getenv("RMBG_API", "a6qxsmMJ3CsNo7HyxuKGsP1o")
 
-MONGO_URL = os.getenv("MONGO_URL", "mongodb+srv://kenzidev3:KenziDev2025@cluster0.d8bxa8a.mongodb.net/?appName=Cluster0
+MONGO_URL = os.getenv("MONGO_URL", "mongodb+srv://kenzidev3:ubotkenzi97@cluster0.d8bxa8a.mongodb.net/pyroubot?retryWrites=true&w=majority
 LOGS_MAKER_UBOT = int(os.getenv("LOGS_MAKER_UBOT", "-1003558835687"))
